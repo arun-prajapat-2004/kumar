b38d6423bb2fbc641096463435f6b555e0b652bb
Arun <arunprajapati2023@gmail.com>
2024-11-25 09:21:23 +0000

    kumar

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmain = "kumar@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2 // not alliwed
+
+accountEmain = "prajapat@gmail.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmain, accountPassword, accountCity])
