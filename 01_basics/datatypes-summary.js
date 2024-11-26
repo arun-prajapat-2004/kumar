9e5c971a1558da0a1c9a37e73da1f748b4d1977c
Arun <arunprajapati2023@gmail.com>
2024-11-26 03:56:34 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
