e831a672ab059c53dc6d6292aa00a899f598b579
Arun <arunprajapati2023@gmail.com>
2024-11-27 04:57:36 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof bigNumber);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non Primitive)
+
+let myYoutubename = "Mr.-kumar-bhai-gamer"
+
+let anothername = myYoutubename
+anothername = "kumar-gamer"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "kumar@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "arun@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
